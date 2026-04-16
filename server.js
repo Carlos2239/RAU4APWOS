1508df459b27f0c7820a406e1efe5288a4685bf5
Carlos2239 <carlosgamer2239@gmail.com>
2026-04-15 22:06:25 -0600

    arreglado

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || 'mysql.railway.internal',
+    host: process.env.MYSQLHOST || 'rau4apwos.railway.internal',
     user: process.env.MYSQLUSER || 'root',
     password: process.env.MYSQLPASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'railway',
